55df4172f9356e661680ba311447656bcb1356aa
Jakob <schasch.dev@protonmail.com>
2022-11-17 23:12:18 +0100

    Added multi line splitting for dirksay

---
+++ utils/speechBubbleUtil.js
@@ -0,0 +1,71 @@
+const LINE_LIMIT = 50;
+const bubbleConnection = "\n    \\\n     \\\n";
+
+function createLineArray(text){
+    let lineArray = [];
+
+    let wordsArray = text.split(" ");
+    let charCounter = 0;
+    let lineArrayIndex = 0;
+
+    let wordSnippetArray = [];
+
+    for (let i = 0; i < wordsArray.length; i++) {
+        if(wordsArray[i].length > LINE_LIMIT + 5){
+            let splitWord = wordsArray[i].match(/.{1,50}/g);
+            wordSnippetArray = wordSnippetArray.concat(splitWord);
+            continue;
+        }
+
+        wordSnippetArray.push(wordsArray[i]);
+    }
+
+    lineArray[0] = wordSnippetArray[0];
+
+    for (let i = 1; i < wordSnippetArray.length; i++) {
+        let currentWord = wordSnippetArray[i];
+        charCounter += currentWord.length;
+
+        if(charCounter + lineArray[lineArrayIndex].length > LINE_LIMIT){
+            lineArrayIndex++;
+            lineArray[lineArrayIndex] = currentWord;
+            charCounter = 0;
+            continue;
+        }
+
+        lineArray[lineArrayIndex] += (" " + currentWord);
+    }
+    return lineArray;
+}
+
+function oneLineBubble(line){
+    let bar = createHorizontalBorder(line.length);
+    return " " + bar + `\n< ${line} >\n ` + bar + bubbleConnection;
+}
+function multiLineBubble(lines){
+    let maxLen = lines.sort((a, b) => {return b.length - a.length})[0].length;
+    let bar = createHorizontalBorder(maxLen);
+    let returnString = " " + bar + `\n/ ${lines[0]} \\\n`;
+    for (let i = 1; i < lines.length - 1; i++) {
+        returnString += `| ${lines[i]} |\n`;
+    }
+    returnString += `\\ ${lines[lines.length - 1]} /\n ` + bar;
+    return returnString;
+}
+
+function createHorizontalBorder(len){
+    let returnString = "";
+    for (let i = 0; i < len + 2; i++) {
+        returnString += "⎯"
+    }
+    return returnString;
+}
+
+function createSpeechBubble(text){
+    let lineArray = createLineArray(text);
+
+    if(lineArray.length > 1) return multiLineBubble(lineArray)
+    return oneLineBubble(lineArray[0]);
+}
+
+module.exports.speechBubbleCreator = createSpeechBubble;
