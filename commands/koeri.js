d9f96610c8b1a22a99ca99c732952223507e432c
Moy <MoritzRoller@outlook.de>
2022-12-19 11:04:55 +0100

    added random combinations of koeri!

--- commands/koeri.js
+++ commands/koeri.js
@@ -1,10 +1,19 @@
 const { SlashCommandBuilder } = require('discord.js');
+const koeri = [1,2,3,4,5,6];
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('kœri')
-		.setDescription('What ist the best kœri?'),
+		.setDescription('Dirk is going to recommend you an excellent kœri combination. Give it a try!'),
 	async execute(interaction) {
-		return interaction.reply('The best kœri is ' + Math.floor(Math.random() * 7) + 'er kœri!');
+		i = Math.floor(Math.random() * 7);
+		const shuffledKoeri = koeri.sort((a, b) => 0.5 - Math.random());
+		const first = shuffledKoeri.slice(0, i);
+		first.sort();
+		if (first == koeri) {
+			return interaction.reply('Today, I can recommend infinity kœri!');
+		} else {
+			return interaction.reply('Today, I can recommend ' + first + 'er kœri!');
+		}
 	},
 };
