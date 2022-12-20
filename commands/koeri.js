1ec1ff2198bab69785365402ed700de211fd3794
Moy <MoritzRoller@outlook.de>
2022-12-20 11:21:40 +0100

    added different messages, as well a fixed rule for infinity koeri

--- commands/koeri.js
+++ commands/koeri.js
@@ -1,19 +1,21 @@
 const { SlashCommandBuilder } = require('discord.js');
 const koeri = [1,2,3,4,5,6];
+const outputs = ['Today, I can recommend ', 'The best kœri is ', 'How about ', 'Why not try the very special ', 'You shall choose '];
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('kœri')
-		.setDescription('Dirk is going to recommend you an excellent kœri combination. Give it a try!'),
+		.setDescription('Dirk is going to recommend you an excellent choice of kœri. Give it a try!'),
 	async execute(interaction) {
-		i = Math.floor(Math.random() * 7);
+		i = Math.floor(Math.random() * 7) + 1;
+		j = Math.floor(Math.random() * (outputs.length));
 		const shuffledKoeri = koeri.sort((a, b) => 0.5 - Math.random());
 		const first = shuffledKoeri.slice(0, i);
 		first.sort();
-		if (first == koeri) {
-			return interaction.reply('Today, I can recommend infinity kœri!');
+		if (first.length == koeri.length) {
+			return interaction.reply(outputs[j] + 'infinity kœri!');
 		} else {
-			return interaction.reply('Today, I can recommend ' + first + 'er kœri!');
+			return interaction.reply(outputs[j] + first + 'er kœri');
 		}
 	},
 };
--- commands/test.js
+++ commands/test.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('test')
-		.setDescription('Checks if Prof Sinz would approve of this bot'),
+		.setDescription('Checks if Prof. Sinz would approve of this bot'),
 	async execute(interaction) {
 		return interaction.reply('Bot is Sinz approved <:sinz_approved:1042760945052692571>');
 	},
