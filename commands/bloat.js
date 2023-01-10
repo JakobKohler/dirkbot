348fc97cfc54abb019d101081a346544f9779a38
Moy <MoritzRoller@outlook.de>
2023-01-10 21:58:01 +0100

    added koeristats and bloat counter

---
+++ commands/bloat.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('bloat')
+		.setDescription('Counts how many times we have said "bloat" this year')
+        .addIntegerOption(option =>
+            option.setName('int')
+                .setDescription('How many "bloats" were bloated')),
+	async execute(interaction) {
+        let bloatNumber = interaction.options.getInteger('int');
+        let jsonObj = JSON.parse(fs.readFileSync("databank/bloatcount.json"));
+        if(bloatNumber === null){
+            jsonObj["bloatCount"]++;
+		}else{
+            jsonObj["bloatCount"] += bloatNumber;
+        }
+        fs.writeFileSync("databank/bloatcount.json", JSON.stringify(jsonObj));
+        return interaction.reply('This year, we have said "bloat" ' + jsonObj["bloatCount"] + ' times.');
+	},
+};
