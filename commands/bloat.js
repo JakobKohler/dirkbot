381b5972d911f61037ad2b87cfa21f92b06cad9c
Jakob Kohler <schasch.dev@protonmail.com>
2023-03-07 00:27:42 +0100

    Added database and implemented bloat counter

--- commands/bloat.js
+++ commands/bloat.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
-const fs = require('fs');
+const {MongoClient, ServerApiVersion} = require('mongodb');
+const { dataBaseURI } = require('../config.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -7,22 +8,43 @@ module.exports = {
 		.setDescription('Counts how many times we have said "bloat" this year')
         .addIntegerOption(option =>
             option.setName('int')
-                .setDescription('How many "bloats" were bloated')),
+                .setDescription('How many "bloats" were bloated')
+                .setRequired(false)),
 	async execute(interaction) {
         const interactionUser = await interaction.guild.members.fetch(interaction.user.id);
+        const client = new MongoClient(dataBaseURI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 		let nickname = interactionUser.nickname;
         let username = interactionUser.user.username;
-        if(nickname === null){
+        if(!nickname){
 			nickname = username;
         }
         let bloatNumber = interaction.options.getInteger('int');
-        let jsonObj = JSON.parse(fs.readFileSync("databank/bloatcount.json"));
-        if(bloatNumber === null){
-            jsonObj["bloatCount"]++;
-		}else{
-            jsonObj["bloatCount"] += bloatNumber;
+        
+
+        await client.connect();
+    
+        if(!bloatNumber) {
+            const count = await readBloatCount(client);
+            return interaction.reply(`Together we have said "bloat ${count} times.`)
+        } else {
+            const updateSuccess = await updateBloatCount(client, bloatNumber);
+            if(updateSuccess){
+                const newCount = await readBloatCount(client);
+                return interaction.reply(`${nickname} bloated again. Together we have said bloat ${newCount} times.`);
+            }else{
+                return interaction.reply('There was an error updating the database');
+            }
         }
-        fs.writeFileSync("databank/bloatcount.json", JSON.stringify(jsonObj));
-        return interaction.reply(nickname + ' bloated again. Together we have said "bloat" ' + jsonObj["bloatCount"] + ' times.');
 	},
 };
+
+async function readBloatCount(client){
+    const bloatCounter = await client.db("dirkbase").collection("generalData").findOne({ name: "bloatCounter" });
+    return bloatCounter.value;
+}
+async function updateBloatCount(client, increment){
+    const updateResult = await client.db("dirkbase").collection("generalData")
+        .updateOne({name: "bloatCounter"}, {$inc : {value: increment}});
+    return updateResult.acknowledged;
+}
