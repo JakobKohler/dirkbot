cb2ff3aee93f202deb8c685bfe715283686d5050
Mofi202 <MoritzRoller@outlook.de>
2023-03-07 14:27:21 +0100

    added scheduled menu message

---
+++ utils/sendMenu.js
@@ -0,0 +1,17 @@
+const config = require('../config.json');
+const { getMenu } = require("../utils/fetchMensaMenu.js");
+const { createMenuEmbed } = require('../utils/buildMenuEmbed.js');
+
+async function sendMenu(client) {
+    const guild = client.guilds.cache.get(config.guildId);
+    const channel = guild.channels.cache.get(config.mensaChannelId);
+    
+    let fetchResults = await getMenu();
+    if(!fetchResults.menuData || fetchResults.menuData.length == 0){
+        return channel.send(`Mensa Moltke seems to be closed today.}`);
+    }
+
+    const menuEmbed = createMenuEmbed(fetchResults.menuData);
+    channel.send({ embeds: [menuEmbed] });
+}
+module.exports.sendMenu = sendMenu;
