e9b275091246be318864b81eba0149bcc4884aae
nils-matthaei <nils.matthaei@gmail.com>
2023-03-13 10:25:06 +0100

    Adjusted weights and added NULLER KOERI
    There is now a 0.5% chance to get INFINITY KOERI and a 0.5% chance to get NULLER KOERI.
    
    Chances for regualar koeri selections have been adjusted accordingly

--- commands/koeri.js
+++ commands/koeri.js
@@ -1,9 +1,9 @@
 const { SlashCommandBuilder } = require('discord.js');
 const koeri = [0,1,2,3,4,5];
-const outputs = ['today I can recommend you ', 'the best kœri is ', 'how about ', 'why not try the very special ', 'you shall choose '];
+const messages = ['today I can recommend you ', 'the best kœri is ', 'how about ', 'why not try the very special ', 'you shall choose '];
 const koeriIDs = ['<:koeri1:1062484320108957801>', '<:koeri2:1062484325276336218>', '<:koeri3:1062484329806176276>', '<:koeri4:1062484331882348585>', '<:koeri5:1062484336013738034>', '<:koeri6:1062484339402756127>'];
 
-const randomArray = [0,1,1,2,2,2,2,3,3,3,3,4,4,5]
+const randomArray = [1,2,2,3,3,3,3,3,4,4,5];
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -21,15 +21,29 @@ module.exports = {
 		const username = interactionUser.user.username;
 
 		//random generation of the koeri combination and output message
-		let i = Math.floor(Math.random() * randomArray.length);
-		let j = Math.floor(Math.random() * (outputs.length));
-		const shuffledKoeri = koeri.sort((a, b) => 0.5 - Math.random());
-		const first = shuffledKoeri.slice(0, randomArray[i] + 1);
-		first.sort();
-		let output = first;
-		for (let k = 0; k < output.length; k++) {
-			output[k] = koeriIDs[output[k]];
+		let koerichance = Math.floor(Math.random()*200);
+		let infinity = false;
+		var j = 0;
+		var output;
+		if(0 < koerichance && koerichance < 199){
+			let i = Math.floor(Math.random() * randomArray.length);
+			j = Math.floor(Math.random() * (messages.length));
+			const shuffledKoeri = koeri.sort((a, b) => 0.5 - Math.random());
+			const slicedKoeri = shuffledKoeri.slice(0, randomArray[i]);
+			slicedKoeri.sort();
+			output = slicedKoeri;
+			for (let k = 0; k < output.length; k++) {
+				output[k] = koeriIDs[output[k]];
 			}
+		}
+		else if(koerichance == 0){
+			output =['<:koeri0:1084759157296615434>'];
+		}
+		else if(koerichance){
+			infinity = true;
+		}
+
+		
 
 		//nickname only used, when normal Discord Username overwritten
 		if(nametext === null && nickname === null){
@@ -38,10 +52,10 @@ module.exports = {
 			nametext = nickname;
 		}
 
-		if (first.length == koeri.length){
-			return interaction.reply(nametext + ', ' + outputs[j] + '<:infinity1:1062613571751661628><:infinity2:1062613573928505364><:infinity3:1062613576956792862><:infinity4:1062613580215754782> kœri!');
+		if (infinity==true){
+			return interaction.reply(nametext + ', ' + messages[j] + '<:infinity1:1062613571751661628><:infinity2:1062613573928505364><:infinity3:1062613576956792862><:infinity4:1062613580215754782> kœri!');
 		} else {
-			return interaction.reply(nametext + ', ' + outputs[j] + output + ' kœri!');
+			return interaction.reply(nametext + ', ' + messages[j] + output + ' kœri!');
 		}
 	},
 };
