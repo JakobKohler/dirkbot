bcc3c7d0e0cb1b9f6c130550edfbbb012ed828eb
Eisern <notmyname.eisern@gmail.com>
2023-04-07 04:18:17 +0200

    Done: - Data saven und readen - Data als alt markieren
    To-Do:
    - entscheidung treffen, ob als binary oder txt file absteichern
    - in format function: encoded snippet und co rausfiltern
    - Discord incorporation
    - Specific Date abfragen
    - Clear Up Code

--- utils/blackTable.js
+++ utils/blackTable.js
@@ -1,6 +1,6 @@
 const  { fetchAndParseFeed }  = require('./rss_parser.js')
+const fs = require('node:fs')
 
-let oldData = {};
 async function fetchData(date) {
     let feed = {};
     let feedURL = 'https://www.iwi.hs-karlsruhe.de/iwii/REST/rssfeed/newsbulletinboard/INFB';
@@ -30,29 +30,67 @@ async function fetchData(date) {
             return feed;
         });
 
+    let oldData = {};
     //alte Data aus neuen Daten entfernen
-    if (!(oldData = {})) {
-        //If there is oldData, filter the new Data to exclude all oldData
-        feed.feedData.items.filter(item => oldData.items.some(obj => obj.title === item.title && obj.isoDate === item.isoDate))
-    } else {
-        oldData = feed.feedData;
-    }
+    fs.readFile('../resources/blackTableData.bin', (err, buffer) => {
+        if (err) {          //If called, file probably empty. That's ok, we take that into consideration
+            console.log(err);
+        } else {
+            //If there is oldData, filter the new Data to exclude all oldData
+            oldData = JSON.parse(buffer.toString());
+            let oldFeed = feed;                 //Debugging
+            feed.feedData.items.filter(item => oldData.items.some(obj => obj.title === item.title && obj.isoDate === item.isoDate))
 
-
-    feed.feedData.items.forEach(item => {
-
-        console.log("\n"+item.isoDate);
-        //although not shown, it is possible to get the subItems from item, like item.title or item.isoDate!
+            console.log(oldFeed.feedData);          //Debugging
+            console.log("\n\n" + feed.feedData);    //Debugging
+        }
     })
 
+
+/*
+    feed.feedData.items.forEach(item => {
+        console.log("\n"+item.isoDate);
+    })
+*/
 
     return feed;
 }
 
-function fetchOldData() {
-    oldData.items.forEach(item => {
-        item.displayed = true;
-    })
+function saveOldData(displayedData) {
+    let oldData = {};
+    //Get oldData from blackTableData.bin
+    fs.readFile('../resources/blacktableData.bin', (err, buffer) => {
+        if (err) {          //If called, file probably empty. That's ok, we take that into consideration
+            console.log(err);
+            oldData = displayedData.feedData;
+        } else {
+            oldData = JSON.parse(buffer.toString());
+            oldData.items.push(displayedData.feedData.items);
+        }
+
+        buffer = Buffer.from(JSON.stringify(oldData));
+        //Save Data to blackTableData.txt
+        fs.writeFile('../resources/blackTableData.bin', buffer, err => {
+            if (err) {
+                console.error('Error writing data to file: blackTableData.bin', err)
+            }
+        })
+
+    });
+
+
 }
-fetchData();
+
+module.exports = {fetchData, saveOldData}
+
+async function getBlackTable() {
+    await fetchData(0).then(data => {
+        saveOldData(data);
+    });
+    return "SUCCESSFUL";
+}
+
+getBlackTable();
+
+//although not shown, it is possible to get the subItems from item, like item.title or item.isoDate!
 
